a26be73cd939619d76b076fe4de68314de860747
ediwaldoneto <ediwaldo.neto@outlook.com>
2020-09-15 20:21:44 -0300

    fix - persistence.xml

--- src/main/java/br/com/curso/jpa/modelo/Conta.java
+++ src/main/java/br/com/curso/jpa/modelo/Conta.java
@@ -1,10 +1,33 @@
 package br.com.curso.jpa.modelo;
 
-public class Conta {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Conta implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private Integer agencia;
 	private Integer numero;
 	private String titular;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public Integer getAgencia() {
 		return agencia;
