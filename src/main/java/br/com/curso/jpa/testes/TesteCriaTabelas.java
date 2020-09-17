17fc6cdec9116c94d586255a5052ae0cbe6da834
ediwaldoneto <ediwaldo.neto@outlook.com>
2020-09-17 15:42:37 -0300

    Associando categorias às movimentações

--- src/main/java/br/com/curso/jpa/testes/TesteCriaTabelas.java
+++ src/main/java/br/com/curso/jpa/testes/TesteCriaTabelas.java
@@ -6,10 +6,7 @@ import javax.persistence.Persistence;
 
 public class TesteCriaTabelas {
 	public static void main(String[] args) {
-		
-		
-		
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
 		EntityManager createEntityManager = emf.createEntityManager();
 		emf.close();
